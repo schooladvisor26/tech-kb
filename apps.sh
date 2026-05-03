574cddfd18ab754c52bdaa75dab0248e1ac2ef03
D <de784@live.com>
2026-05-03 16:49:12 -0700

    add apps show script

---
+++ apps.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# apps.sh
+# Make sure to run "bash ./nameofscript.sh" replacing nameofscript.sh with the name of the this script.
+
+touch apt-show-new-2026.txt
+
+echo "apps-of-interest" >> apt-show-new-2026.txt
+echo " " >> apt-show-new-2026.txt
+
+apt show ephoto  >> apt-show-mokshas-2026.txt
+apt show exfatprogs  >> apt-show-mokshas-2026.txt
+apt show fastfetch  >> apt-show-mokshas-2026.txt
+apt show flameshot  >> apt-show-mokshas-2026.txt
+apt show geany  >> apt-show-mokshas-2026.txt
+apt show gnome-paint  >> apt-show-mokshas-2026.txt
+apt show gnome-boxes  >> apt-show-mokshas-2026.txt
+apt show libreoffice  >> apt-show-mokshas-2026.txt
+apt show micro  >> apt-show-mokshas-2026.txt
+apt show neofetch  >> apt-show-mokshas-2026.txt
+apt show qpdfview >> apt-show-mokshas-2026.txt
+apt show rage  >> apt-show-mokshas-2026.txt
+apt show rednotebook  >> apt-show-mokshas-2026.txt
+apt show sq  >> apt-show-mokshas-2026.txt
+apt show unetbootin  >> apt-show-mokshas-2026.txt
+apt show xsane  >> apt-show-mokshas-2026.txt
