9995eb5fff4043f9a5fc1d453d62291ea858d261
schooladvisor26 <de784@live.com>
2026-07-10 16:17:19 -0700

    updates check sys script

--- checksystem.sh
+++ checksystem.sh
@@ -2,14 +2,18 @@
 # checksystem.sh
 # Make sure to run "bash ./nameofscript.sh" replacing nameofscript.sh with the name of the this script.
 
+# Creates system report as a plain text file
 touch system-info.txt
 
+# Prints the words system info 
 echo "system-info" >> system-info.txt
 
+# Prints the date at the top (should include the time zone)
 echo "date:" >> system-info.txt
 date >> system-info.txt
 echo " " >> system-info.txt
 
+# Prints inforomation about Kernel and other things
 echo "kernel and other info:" >> system-info.txt
 uname -a >> system-info.txt
 echo " " >> system-info.txt
@@ -17,5 +21,11 @@ echo " " >> system-info.txt
 df -h >> system-info.txt
 echo " " >> system-info.txt
 
+# Prints numper of software packages (installed via apt)
+# Note that that's an "l" after wc (word count) NOT a "1" (one)
 echo "number of packages:" >> system-info.txt
 apt list --installed | wc -l >> system-info.txt
+
+# Prints OS distribution, version, and any code names
+echo "os distribution, version, and any code names"
+cat /etc/os-release >> system-info.txt
